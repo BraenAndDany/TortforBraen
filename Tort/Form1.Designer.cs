f876a484c3ff6ebc8b503f8a73c82f456a51d717
BraenAndDany <braenanddany@mail.ru>
2023-09-26 14:15:38 +0700

    Добавьте файлы проекта.

---
+++ Tort/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Tort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(347, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-2, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(790, 412);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox2_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Tort/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tort
+{
+    public partial class Form1 : Form
+    {
+        public Form1() 
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = CreateGraphics();
+            g.Clear(Color.Black);
+            g.FillRectangle(Brushes.BurlyWood, 300, 300, 200, 50);
+            g.FillRectangle(Brushes.Pink, 295, 280, 210, 30);
+            g.FillRectangle(Brushes.BurlyWood, 300, 240, 200, 40);
+            g.FillRectangle(Brushes.Pink, 295, 210, 210, 30);
+            g.FillRectangle(Brushes.BurlyWood, 300, 170, 200, 40);
+            g.FillRectangle(Brushes.White, 290, 150, 220, 20);
+            g.FillRectangle(Brushes.White, 290, 150, 10, 50);
+            g.FillRectangle(Brushes.White, 320, 150, 10, 60);
+            g.FillRectangle(Brushes.White, 350, 150, 10, 70);
+            g.FillRectangle(Brushes.White, 350, 150, 30, 30);
+            g.FillRectangle(Brushes.White, 300, 150, 20, 30);
+            g.FillRectangle(Brushes.White, 370, 150, 10, 60);
+            g.FillRectangle(Brushes.White, 390, 150, 10, 50);
+            g.FillRectangle(Brushes.White, 400, 150, 20, 30);
+            g.FillRectangle(Brushes.White, 430, 150, 10, 70);
+            g.FillRectangle(Brushes.White, 450, 150, 10, 70);
+            g.FillRectangle(Brushes.White, 460, 150, 30, 30);
+            g.FillRectangle(Brushes.White, 480, 150, 20, 30);
+            g.FillRectangle(Brushes.White, 480, 150, 10, 70);
+            g.FillRectangle(Brushes.Red, 320, 130, 20, 20);
+            g.FillRectangle(Brushes.Red, 360, 130, 20, 20);
+            g.FillRectangle(Brushes.Red, 420, 130, 20, 20);
+            g.FillRectangle(Brushes.Red, 460, 130, 20, 20);
+            g.FillRectangle(Brushes.Gray, 395, 70, 10, 80);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox2_DoubleClick(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true; 
+            pictureBox2.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox2.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = true;
+            pictureBox2.Show();
+        }
+    }
+}
